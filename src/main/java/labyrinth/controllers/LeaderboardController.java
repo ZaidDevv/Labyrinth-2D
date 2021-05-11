0d4643ba1dddce782d2620a1ecc386ecc3033d9d
Zaid Hameed <zaidbusiness@hotmail.com>
2021-05-11 16:12:32 +0200

    Designed Leaderboards UI

---
+++ src/main/java/labyrinth/controllers/LeaderboardController.java
@@ -0,0 +1,9 @@
+package labyrinth.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class LeaderboardController {
+    @FXML
+    TableView tvLeaderboard;
+}
